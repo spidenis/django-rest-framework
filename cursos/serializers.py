0439ab4bfb981db4593c9dfab823b0ab92928b48
Valdenilson de Melo Nascimento <valdenilson.melo@ifac.edu.br>
2024-10-25 14:02:34 -0500

    --feat: criado relacionamento entre curso e avaliacoes

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -12,7 +12,26 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class CursoSerializer(serializers.ModelSerializer):
+    #Nested Relationship (Traz o objeto DJSON)
+    #avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
     
+    #HyperLinked Related Field (Link de acesso ao recurso)
+    """
+    avaliacoes = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='avaliacao-detail'
+    )    
+    """
+    #Primary Key Related Field (Chaves primarias - Maxima performance)
+    avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
         model = Curso
-        fields = '__all__'
+        fields = (
+            'id',
+            'titulo',
+            'url',
+            'criacao',
+            'ativo',
+            'avaliacoes'
+        )
