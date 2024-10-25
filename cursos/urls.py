7b361d4e99fa6228706e664de0d83ab1e92dcd15
Valdenilson de Melo Nascimento <valdenilson.melo@ifac.edu.br>
2024-10-25 11:42:17 -0500

    --feat: criando API V2 com ViewSet

--- cursos/urls.py
+++ cursos/urls.py
@@ -1,5 +1,18 @@
 from django.urls import path
-from .views import CursosAPIView, AvaliacoesAPIView, CursoAPIView, AvaliacaoAPIView
+from rest_framework.routers import SimpleRouter
+
+from .views import (
+    CursosAPIView, 
+    AvaliacoesAPIView, 
+    CursoAPIView,
+    AvaliacaoAPIView,
+    CursoViewSet,
+    AvaliacaoViewSet,
+    )
+
+router = SimpleRouter()
+router.register('cursos', CursoViewSet)
+router.register('avaliacoes', AvaliacaoViewSet)
 
 urlpatterns = [
     path('cursos/', CursosAPIView.as_view(), name='cursos'),
