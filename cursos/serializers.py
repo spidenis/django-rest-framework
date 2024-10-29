7f501331f43fdc97dd94f6ce01b300ac03f6788a
Valdenilson de Melo Nascimento <valdenilson.melo@ifac.edu.br>
2024-10-29 16:53:41 -0500

    --feat: criando campo de media de avaliação calculada via função no serializer

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers #Trasnformar objetos python em JSON e vice versa
+from django.db.models import Avg
+
 from .models import Curso, Avaliacao
 
 class AvaliacaoSerializer(serializers.ModelSerializer):
@@ -9,8 +11,22 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
             'email': {'write_only':True}
         }
         model = Avaliacao
-        fields = '__all__'
+        fields = (
+            'id',
+            'curso',
+            'nome',
+            'email',
+            'comentario',
+            'avaliacao',
+            'criacao',
+            'ativo',
+        )
 
+    def validate_avaliacao(self, valor):
+        if valor in range(1,6):
+            return valor
+        raise serializers.ValidationError('A avaliação precisa ser um inteiro entre 1 e 5')
+    
 class CursoSerializer(serializers.ModelSerializer):
     #Nested Relationship (Traz o objeto DJSON)
     #avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
@@ -25,6 +41,9 @@ class CursoSerializer(serializers.ModelSerializer):
     """
     #Primary Key Related Field (Chaves primarias - Maxima performance)
     avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    media_avaliacoes = serializers.SerializerMethodField()
+
     class Meta:
         model = Curso
         fields = (
@@ -33,5 +52,13 @@ class CursoSerializer(serializers.ModelSerializer):
             'url',
             'criacao',
             'ativo',
-            'avaliacoes'
+            'avaliacoes',
+            'media_avaliacoes'
         )
+    
+    def get_media_avaliacoes(self, obj):
+        media = obj.avaliacoes.aggregate(Avg('avaliacao')).get('avaliacao__avg')
+
+        if media is None:
+            return 0
+        return round(media * 2) / 2
